ad0ebdb16a98c5c63c92cb90c9c668600b7450b6
Gladson <gladsonfinamore@gmail.com>
2021-10-10 19:06:33 -0300

    trabalho concluido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trab-uninter';
+  title = 'Uninter';
+  data: Array<any> | undefined;
+
+  constructor(){
+    this.data = [
+      {
+        "Name" : "Gladson",
+        "RU" : "3285207",
+        "Curso" : "Analise e Desenvolvimento de sistemas",
+        "Data" : "05/09/1997"
+      },
+      {
+        "Name" : "Lucas",
+        "RU" : "0385207",
+        "Curso" : "Administrção",
+        "Data" : "03/10/1992"
+      },
+      {
+        "Name" : "Mauricio",
+        "RU" : "0585207",
+        "Curso" : "Portugues",
+        "Data" : "01/01/1995"
+      },
+      {
+        "Name" : "Clara",
+        "RU" : "0085207",
+        "Curso" : "Matemática",
+        "Data" : "06/07/1998"
+      }
+    ]
+  }
 }
